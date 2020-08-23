6383ca3f8041f250a8e17e07badc32e1551783d4
MERN Stack Master <msm@MBP.local>
2020-08-23 13:41:09 -0700

    Updates

---
+++ precache-manifest.15e0204c25251159113640b91ee518f1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64df8db3eb64627eef2ccdae2b465c73",
+    "url": "/insult-generator/index.html"
+  },
+  {
+    "revision": "abb9c287de4602de1d00",
+    "url": "/insult-generator/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c1cbc5ac690813dc19c6",
+    "url": "/insult-generator/static/js/2.e25d3333.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/insult-generator/static/js/2.e25d3333.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "abb9c287de4602de1d00",
+    "url": "/insult-generator/static/js/main.1c275919.chunk.js"
+  },
+  {
+    "revision": "38e4bee8312850c20051",
+    "url": "/insult-generator/static/js/runtime-main.29f793d3.js"
+  }
+]);
