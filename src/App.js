7bc1107bc9be3a9d4c3eb42aa767318d3e87bb65
MERN Stack Master <msm@MBP.local>
2020-08-23 13:38:28 -0700

    worst generator ever done

--- src/App.js
+++ src/App.js
@@ -1,21 +1,71 @@
 import React from 'react';
-// import logo from './logo.svg';
+import words from './words/';
 import './App.css';
 
-function App() {
-	return (
-		<div className="container">
-		<div className="row">
-			<div className="jumbotron">
-				<h1 className="display-4">Hello, world!</h1>
-				<p className="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
-				<hr className="my-4" />
-				<p>It uses utility classNamees for typography and spacing to space content out within the larger container.</p>
-				<a className="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+class App extends React.Component {
+	constructor() {
+		super();
+
+		this.vowels = ['a', 'e', 'i', 'o', 'u'];
+
+		this.state = {
+			name: '',
+			adj1: '',
+			adj2: ''
+		};
+	}
+
+	componentDidMount() {
+		// console.log(words.nouns);
+	}
+
+	handleInputChange = (e) => {
+		const { name, value } = e.target;
+
+		this.getAllWords();
+
+		this.setState({ [name]: value });
+	};
+
+	getRandomWord = (wordArr, letter) => {
+		const filteredWords = wordArr.filter((word) => word[0] === letter);
+		return filteredWords[Math.floor(Math.random() * filteredWords.length)]
+	};
+
+	getRandomLetter = () => String.fromCharCode(Math.floor(Math.random() * (122 - 97) + 97));
+
+	getAllWords = () => {
+		this.setState({
+			adj1: this.getRandomWord(words.adjectives, this.getRandomLetter()),
+			adj2: this.getRandomWord(words.adjectives, this.getRandomLetter()),
+			nou1: this.getRandomWord(words.nouns, this.getRandomLetter()),
+			nou2: this.getRandomWord(words.nouns, this.getRandomLetter())
+		});
+		;
+	};
+
+	render() {
+		return (
+			<div className="container">
+			<div className="row">
+				<div className="jumbotron" style={{width: '100%'}}>
+					<h1 className="display-4">Insult Generator</h1>
+					<label>
+						<input onChange={this.handleInputChange} type="text" name="name" value={this.state.name} placeholder="Who is your enemy?" />
+					</label>
+					{this.state.name ?
+					<p
+						className="lead"
+					>
+						{this.state.name} is {this.vowels.includes(this.state.adj1[0]) ?
+							'an' :
+							'a'} {this.state.adj1} {this.state.adj2} {this.state.nou1} {this.state.nou2}.</p> :
+					null}
+				</div>
 			</div>
 		</div>
-	</div>
-	);
+		);
+	}
 }
 
 export default App;
---
+++ src/words/index.js
@@ -0,0 +1,4 @@
+import nouns from './nouns';
+import adjectives from './adjectives';
+
+export default { nouns, adjectives };
